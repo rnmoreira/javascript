7a05d99d879299a083926e69473cb7f7692d00c6
Renan Almeida <renan68@outlook.com>
2022-06-09 23:22:28 -0300

    Primeiro projeto de Javascript

---
+++ Projeto 1/desafio.js
@@ -0,0 +1,11 @@
+var recebe_nome = "Renan";
+var recebe_number = 29;
+var ano_subtrai=2022-recebe_number;
+
+console.log(recebe_number+ano_subtrai)
+console.log("Estou mexendo no console.log")
+
+var quartaFeira = false;
+
+console.log(quartaFeira)
+console.log(typeof(quartaFeira))
